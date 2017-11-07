f4ae21b1c13a1f157be3aa523e7d0237d1e91d57
Shawn Olson <LOCODARWIN@msn.com>
2017-11-07 12:23:51 -0800

    Latest 11-17

--- RWXLightReplace/Form1.Designer.cs
+++ RWXLightReplace/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.texDir = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(656, 25);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(38, 177);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(226, 24);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Unzip and Process Any Zips";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(38, 212);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(171, 24);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Zip After Processing";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Model Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "New Ambient:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "New Specular:";
+            // 
+            // texDir
+            // 
+            this.texDir.Location = new System.Drawing.Point(140, 28);
+            this.texDir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.texDir.Name = "texDir";
+            this.texDir.Size = new System.Drawing.Size(505, 26);
+            this.texDir.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 72);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 117);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(426, 128);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 75);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Process!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(783, 257);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.texDir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "RWXLightReplace";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox texDir;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- RWXLightReplace/Form1.cs
+++ RWXLightReplace/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Collections;
 
 namespace RWXLightReplace
 {
@@ -16,5 +19,128 @@ namespace RWXLightReplace
         {
             InitializeComponent();
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openDir = new FolderBrowserDialog();
+            openDir.RootFolder = Environment.SpecialFolder.Desktop;
+
+            if (openDir.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Place the dir into the text box
+                    texDir.Text = openDir.SelectedPath;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read directory from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string ext;
+
+            string FileIn = texDir.Text;
+            if (!Directory.Exists(FileIn))
+            {
+                MessageBox.Show("Directory doesn't exist! Try again.");
+                return;
+            }
+
+
+            // first unzip any zipped files and delete the zip, leaving only the zip's contents
+            string[] files1 = Directory.GetFiles(FileIn);
+
+            foreach (string x in files1)
+            {
+                ext = Path.GetExtension(x);
+                if (ext == ".zip")
+                {
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(x, FileIn);
+                        File.Delete(x);
+                    }
+                    catch {}
+                    
+                }
+
+            }
+
+
+
+            // re-load all the files from the directory into a string
+            string[] files2 = Directory.GetFiles(FileIn);
+
+            
+            
+            foreach (string x in files2)
+            {
+                Iterate(x, FileIn);
+            }
+
+
+
+
+
+
+        }
+
+
+        private void Iterate(string file, string dir)
+        {
+
+            //Console.WriteLine(file);
+            string ext = Path.GetExtension(file);
+
+            // if rwx, process
+            if (ext == ".rwx")
+            {
+                Process(file, dir);
+                return;
+            }
+
+
+           
+            
+
+            // if we processed the file, re-zip it (first, for each file deleteing existing zip that has same filename
+
+
+
+        }
+
+        private void Process(string file, string dir)
+        {
+            Console.WriteLine(file);
+
+
+            // create a new temp file for the RWX copying
+
+            // iterate through each line of the RWX file and replace the light values in the correct tags
+
+            // tag could look like     Surface 1 1 0
+            // or ambient 1   or diffuse 1
+
+            // 
+
+
+
+
+        }
+
+
+        // Documentations
+
+        // StringContains() help & example
+        // https://msdn.microsoft.com/en-us/library/dy85x1sa(v=vs.110).aspx
+
+        //
+
     }
 }
