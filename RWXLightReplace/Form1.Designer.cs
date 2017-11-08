724d386efa635abfa4d1038b84f950e7ec9e5fa6
Shawn Olson <LOCODARWIN@msn.com>
2017-11-07 16:17:51 -0800

    11-7 v2

--- RWXLightReplace/Form1.Designer.cs
+++ RWXLightReplace/Form1.Designer.cs
@@ -42,10 +42,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(656, 25);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(437, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,10 +53,9 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(38, 177);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Location = new System.Drawing.Point(25, 115);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(226, 24);
+            this.checkBox1.Size = new System.Drawing.Size(159, 17);
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "Unzip and Process Any Zips";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -65,10 +63,9 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(38, 212);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox2.Location = new System.Drawing.Point(25, 138);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(171, 24);
+            this.checkBox2.Size = new System.Drawing.Size(121, 17);
             this.checkBox2.TabIndex = 2;
             this.checkBox2.Text = "Zip After Processing";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -76,64 +73,57 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 32);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(27, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Model Path:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 77);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(17, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "New Ambient:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 122);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(20, 79);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.Size = new System.Drawing.Size(68, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "New Specular:";
+            this.label3.Text = "New Diffuse:";
             // 
             // texDir
             // 
-            this.texDir.Location = new System.Drawing.Point(140, 28);
-            this.texDir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.texDir.Location = new System.Drawing.Point(93, 18);
             this.texDir.Name = "texDir";
-            this.texDir.Size = new System.Drawing.Size(505, 26);
+            this.texDir.Size = new System.Drawing.Size(338, 20);
             this.texDir.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(140, 72);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Location = new System.Drawing.Point(93, 47);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(108, 26);
+            this.textBox2.Size = new System.Drawing.Size(73, 20);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(140, 117);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox3.Location = new System.Drawing.Point(93, 76);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(108, 26);
+            this.textBox3.Size = new System.Drawing.Size(73, 20);
             this.textBox3.TabIndex = 8;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(426, 128);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Location = new System.Drawing.Point(284, 83);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 75);
+            this.button2.Size = new System.Drawing.Size(147, 49);
             this.button2.TabIndex = 9;
             this.button2.Text = "Process!";
             this.button2.UseVisualStyleBackColor = true;
@@ -141,9 +131,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 257);
+            this.ClientSize = new System.Drawing.Size(522, 167);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -154,7 +144,6 @@
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "RWXLightReplace";
             this.ResumeLayout(false);
--- RWXLightReplace/Form1.cs
+++ RWXLightReplace/Form1.cs
@@ -20,6 +20,14 @@ namespace RWXLightReplace
             InitializeComponent();
         }
 
+        public static class Globals
+        {
+            public static bool UnzipBefore;
+            public static bool ZipAfter;
+            public static string Ambient;
+            public static string Diffuse;
+
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -52,41 +60,63 @@ namespace RWXLightReplace
                 return;
             }
 
-
-            // first unzip any zipped files and delete the zip, leaving only the zip's contents
-            string[] files1 = Directory.GetFiles(FileIn);
-
-            foreach (string x in files1)
+            // read in the checkboxes
+            if (checkBox1.Checked)
             {
-                ext = Path.GetExtension(x);
-                if (ext == ".zip")
-                {
-                    try
-                    {
-                        ZipFile.ExtractToDirectory(x, FileIn);
-                        File.Delete(x);
-                    }
-                    catch {}
-                    
-                }
-
+                Globals.UnzipBefore = true;
+            }
+            else
+            {
+                Globals.UnzipBefore = false;
             }
 
+            if (checkBox2.Checked)
+            {
+                Globals.ZipAfter = true;
+            }
+            else
+            {
+                Globals.ZipAfter = false;
+            }
+
+            // read in the textboxes for Ambient and Diffuse
+            Globals.Ambient = textBox2.Text;
+            Globals.Diffuse = textBox3.Text;
 
 
-            // re-load all the files from the directory into a string
+            // If the checkbox to unzip before processing is checked...
+            if (Globals.UnzipBefore)
+            {
+                // Unzip any zipped files and delete the zip, leaving only the zip's contents
+                // Will skip any zips that contain duplicate RWXs (fix later)
+                string[] files1 = Directory.GetFiles(FileIn);
+
+                foreach (string x in files1)
+                {
+                    ext = Path.GetExtension(x);
+                    if (ext == ".zip")
+                    {
+                        try
+                        {
+                            ZipFile.ExtractToDirectory(x, FileIn);
+                            File.Delete(x);
+                        }
+                        catch { }
+
+                    }
+
+                }
+            }
+            
+
+            // Load all the files from the directory into a string
             string[] files2 = Directory.GetFiles(FileIn);
-
             
-            
+            // Start the processing
             foreach (string x in files2)
             {
                 Iterate(x, FileIn);
             }
-
-
-
-
 
 
         }
@@ -118,11 +148,79 @@ namespace RWXLightReplace
         private void Process(string file, string dir)
         {
             Console.WriteLine(file);
+            string line, replacewith, linebuild, linepad;
+            int found, index;
+
+            //StringComparison comp = StringComparison.Ordinal;
+
+            // create a new temp file for the RWX output
+            System.IO.StreamWriter outfile = new System.IO.StreamWriter(dir + "\\output.txt", true);
+
+            // open the input file for reading
+            System.IO.StreamReader sfile = new System.IO.StreamReader(file);
+
+            // read each line, and push to the temp file; if the line contains a surface, ambient, or diffuse tag, update it
+            while ((line = sfile.ReadLine()) != null)
+            {
+                found = 0;
+                replacewith = "";
+                index = 0;
+                linepad = "";
+                if (line.Contains("Surface"))
+                {
+                    // find the position of the tag in the string
+                    found = 1;
+                    index = line.IndexOf("Surface");
+                    replacewith = "Surface";
+                }
+                else if (line.Contains("surface"))
+                {
+                    found = 1;
+                    index = line.IndexOf("surface");
+                    replacewith = "Surface";
+                }
+
+                // If the line contains one of the tags, build the new tag and write it to outfile - else write the old line to outfile
+                if (found == 1)
+                {
+
+                    linebuild = linepad.PadRight(index);
+                    linebuild = linebuild + replacewith;
+
+                    if (replacewith == "Surface")
+                    {
+                        linebuild = linebuild + " " + Globals.Ambient + " " + Globals.Diffuse + " 0";
+                    }
+                    else if (replacewith == "Ambient")
+                    {
+                        linebuild = linebuild + " " + Globals.Ambient;
+                    }
+                    else
+                    {
+                        linebuild = linebuild + " " + Globals.Diffuse;
+                    }
+
+                    outfile.WriteLine(linebuild);
+                    
+                }
+                else
+                {
+                    outfile.WriteLine(line);
+                }
 
 
-            // create a new temp file for the RWX copying
+                
 
-            // iterate through each line of the RWX file and replace the light values in the correct tags
+
+            }
+            sfile.Close();
+            outfile.Close();
+
+
+            // After both files are closed, delete the input file and rename the output file to input file
+
+
+
 
             // tag could look like     Surface 1 1 0
             // or ambient 1   or diffuse 1
